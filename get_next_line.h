526445666f27b3b2ad4604fee5b9b3a2fd003be6
Владислав Кунгурцев <vlados_paperos@MacBook-Pro-Vladislav.local>
2020-12-04 12:49:58 +0300

    added bonus and removed garbage

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: auhoris <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/23 12:26:43 by auhoris           #+#    #+#             */
-/*   Updated: 2020/12/01 19:59:46 by auhoris          ###   ########.fr       */
+/*   Updated: 2020/12/04 12:49:22 by auhoris          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,18 @@
 # include <stdlib.h>
 # include <unistd.h>
 
-size_t	ft_strlen(const char *s);
-char	*ft_strjoin(char const *s1, char const *s2);
-char	*ft_strnew(size_t n);
-char	*ft_strdup(const char *s1);
-char	*ft_strncpy(char *dst, char *src, size_t len);
-int		get_next_line(int fd, char **line);
+typedef struct	s_gnl
+{
+	int				fd;
+	char			*reminder;
+	struct s_gnl	*next;
+}				t_gnl;
+
+size_t			ft_strlen(const char *s);
+char			*ft_strjoin(char const *s1, char const *s2);
+char			*ft_strdup(const char *s1);
+int				get_next_line(int fd, char **line);
+char			*ft_nlpos(char *str);
+t_gnl			*new_elem(int fd);
 
 #endif
